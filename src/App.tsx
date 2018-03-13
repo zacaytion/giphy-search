1905954b8d9d75f88981da6ebc40fc3580bc95fa
Zach Harris <z@zharr.is>
2018-03-13 02:30:51 -0400

    set up react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+const App: React.SFC = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  );
+};
+
+export default hot(module)(App);
