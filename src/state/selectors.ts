0b3c8ef6c7f0b3c3e70649bf618f676143f49432
Zach Harris <z@zharr.is>
2018-03-17 00:44:52 -0400

    add search selectors

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -4,22 +4,32 @@ import { GIFTypes } from './actionTypes';
 import { IAppState, IGIFSState, IPaginationState } from './initial';
 import TypeKeys from './typeKeys';
 
-const fetchingStateSelector = (state: IAppState) => state.fetching;
 const gifsStateSelector = (state: IAppState): IGIFSState => state.gifs;
-const paginationStateSelector = (state: IAppState) => state.pagination;
-
 export const gifsSelector = (type: GIFTypes ) => {
   return createSelector(
     gifsStateSelector, gifsState => gifsState[type],
   );
 };
 
+const paginationStateSelector = (state: IAppState) => state.pagination;
 export const paginationSelector = (type: GIFTypes) => {
   return createSelector(
     paginationStateSelector, pageState => pageState[type],
   );
 };
 
+const searchingStateSelector = (state: IAppState) => state.searching;
+export const previousSearchesSelector = createSelector(
+  searchingStateSelector,
+  searching => searching.previous,
+);
+export const currentSearchSelector = createSelector(
+  searchingStateSelector,
+  searching => searching.current,
+);
+
+// TODO: Is Necessary?
+const fetchingStateSelector = (state: IAppState) => state.fetching;
 export const isFetchingSelector = createSelector(
   fetchingStateSelector,
   fetchingState => fetchingState.isFetching,
