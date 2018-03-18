56a9e7926859be07ffd499ee588409b1fb30d1ed
Zach Harris <z@zharr.is>
2018-03-18 02:50:03 -0400

    fetch trending gifs on load

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,27 +1,33 @@
+/* tslint:disable:ordered-imports */
 import { History } from 'history';
+import { hot } from 'react-hot-loader'; // Must Come Before React!!!!!!
 import * as React from 'react';
-import { hot } from 'react-hot-loader';
 import { connect, Provider } from 'react-redux';
 import { Route, Switch, withRouter } from 'react-router';
 import { ConnectedRouter } from 'react-router-redux';
 import { Store } from 'redux';
 import { About, NotFound } from '../components';
 import { Home } from '../containers/Home';
+import { trendingGIFs, TTrendingGIFs } from '../state/actionCreators';
 import { IAppState } from '../state/initial';
 
 interface IProps {
   store: Store<IAppState>;
   history: History;
 }
-interface IState {
+interface IStateProps {
   error: Error | null;
 }
 
-class RootComponent extends React.Component<IProps, IState> {
+class RootComponent extends React.Component<IProps, IStateProps> {
   public state = {
     error: null,
   };
 
+  public componentWillMount() {
+    const { store } = this.props;
+    store.dispatch(trendingGIFs());
+  }
   public componentDidCatch(error: Error) {
     this.setState({ error });
   }
@@ -51,6 +57,8 @@ class RootComponent extends React.Component<IProps, IState> {
     );
   }
 }
-const Root = connect<{}, {}>(null, {})(RootComponent);
+
+const Root = connect<{}, {}>(null, {
+})(RootComponent);
 
 export default hot(module)(Root);
